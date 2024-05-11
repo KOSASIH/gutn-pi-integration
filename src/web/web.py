77c27aa981750a1a591deba38299f0c6016b4df9
KOSASIH <kosasihg88@gmail.com>
2024-05-11 11:46:33 +0700

    Create web.py

---
+++ src/web/web.py
@@ -0,0 +1,10 @@
+# web/web.py
+from flask importFlask
+
+class Web:
+    def __init__(self):
+        self.app = Flask(__name__)
+
+    def start(self):
+        # Implement web interface logic
+        pass
