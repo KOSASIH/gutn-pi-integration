de36220dc3d6ad434742c77901599d28d39ffbc7
KOSASIH <kosasihg88@gmail.com>
2024-05-11 11:44:02 +0700

    Create plugin_loader.py

---
+++ src/plugins/plugin_loader.py
@@ -0,0 +1,10 @@
+# plugins/plugin_loader.py
+import importlib
+
+class PluginLoader:
+    def __init__(self, plugin_dir: str):
+        self.plugin_dir = plugin_dir
+
+    def load_plugins(self):
+        # Implement plugin loading logic
+        pass
