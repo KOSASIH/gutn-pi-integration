e016a7e67884c6a309cc45eeadffd0a76b34ce02
KOSASIH <kosasihg88@gmail.com>
2024-05-11 11:34:20 +0700

    Create temperature_sensor.py

---
+++ src/modules/sensor_modules/temperature_sensor.py
@@ -0,0 +1,10 @@
+# sensor_modules/temperature_sensor.py
+import logging
+
+class TemperatureSensor:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def read_temperature(self):
+        # Implement temperature sensor reading logic
+        pass
