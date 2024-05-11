c4761a5ccd7c3740a155dd97c2e2824cbd9227dd
KOSASIH <kosasihg88@gmail.com>
2024-05-11 11:42:46 +0700

    Create utils.py

---
+++ src/utils/utils.py
@@ -0,0 +1,10 @@
+# utils/utils.py
+import logging
+
+class Utils:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def calculate_average(self, data):
+        # Implement data averaging logic
+        pass
